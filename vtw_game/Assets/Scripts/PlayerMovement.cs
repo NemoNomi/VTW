0d7f331c33aea5d7ab83c07ee758c4e68aee97ce
NemoNomi <noemi.gangl00@gmail.com>
2024-04-02 23:32:03 +0200

    refactoring of PlayerMovement Script

--- vtw_game/Assets/Scripts/PlayerMovement.cs
+++ vtw_game/Assets/Scripts/PlayerMovement.cs
@@ -1,49 +1,58 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Stats")]
     public PlayerStats playerStats;
+
 
     [Header("Physics Components")]
     public Rigidbody2D rb;
     public Transform groundCheck;
     public Transform wallCheckUpper;
     public Transform wallCheckLower;
-
     public LayerMask groundLayer;
     public LayerMask wallLayer;
+
 
     [Header("Anchoring Settings")]
     private bool isAnchored = false;
     private float originalGravityScale;
 
+
     [Header("Climbing Settings")]
     private bool isClimbing = false;
     private float vertical;
 
+
     [Header("Swing Settings")]
-    private float swingCooldown = 0.5f;
+    private float swingCooldown = 1f;
     private float lastJumpTime;
+
 
     [Header("Jump Settings")]
     private bool isJumping;
+
 
     [Header("Timing Settings")]
     private float coyoteTimeCounter;
     private float jumpBufferCounter;
     private float climbBufferCounter;
 
+
     [Header("Player State")]
     private float horizontal;
     private bool isGroundedThisFrame;
     private float lastHorizontalDirection = 0f;
 
+
     void Start()
     {
         originalGravityScale = rb.gravityScale;
     }
+
 
     void Update()
     {
@@ -52,12 +61,14 @@ public class PlayerMovement : MonoBehaviour
         HandlePlayerDirection();
     }
 
+
     private void FixedUpdate()
     {
         HandleMovement();
     }
 
-    #region Ground Checks
+
+    #region GroundChecks
     void UpdateGroundedStatus()
     {
         isGroundedThisFrame = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
@@ -71,14 +82,10 @@ public class PlayerMovement : MonoBehaviour
             coyoteTimeCounter -= Time.deltaTime;
         }
     }
-
-    bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
     #endregion
 
-    #region Timer Updates
+
+    #region TimerUpdates
     void UpdateTimers()
     {
         if (jumpBufferCounter > 0) jumpBufferCounter -= Time.deltaTime;
@@ -86,7 +93,8 @@ public class PlayerMovement : MonoBehaviour
     }
     #endregion
 
-    #region Player Direction
+
+    #region PlayerDirection
     void HandlePlayerDirection()
     {
         if ((horizontal > 0.01f && lastHorizontalDirection <= 0) || (horizontal < -0.01f && lastHorizontalDirection >= 0))
@@ -97,77 +105,87 @@ public class PlayerMovement : MonoBehaviour
     }
     #endregion
 
-    #region Movement Handling
+
+    #region MovementHandling
     void HandleMovement()
     {
-        if (playerStats.canMove && !isAnchored)
+         if (playerStats.canMove && !isAnchored)
         {
-            bool nextToWall = IsNextToWall();
-            bool isWallAbove = Physics2D.OverlapCircle(wallCheckUpper.position, 0.1f, wallLayer);
-            bool isWallBelow = Physics2D.OverlapCircle(wallCheckLower.position, 0.1f, wallLayer);
-
-            if (!isClimbing && climbBufferCounter > 0 && nextToWall)
-            {
-                StartClimbing();
-            }
-
-            if (isClimbing && nextToWall)
-            {
-                rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-                rb.gravityScale = 0;
-
-                float currentClimbingSpeed = 0f;
-                if (vertical > 0 && isWallAbove)
-                {
-                    currentClimbingSpeed = playerStats.climbingSpeedUp;
-                }
-                else if (vertical < 0 && isWallBelow)
-                {
-                    currentClimbingSpeed = playerStats.climbingSpeedDown;
-                }
-
-                rb.velocity = new Vector2(0, vertical * currentClimbingSpeed);
-            }
-            else if (!nextToWall)
-            {
-                StopClimbing();
-            }
+            HandleClimbing(); 
 
             if (!isClimbing)
             {
-                if (isGroundedThisFrame || Time.time - lastJumpTime < swingCooldown)
-                {
-                    rb.velocity = new Vector2(horizontal * playerStats.speed, rb.velocity.y);
-                }
-                else if (Mathf.Abs(horizontal) > 0.01f)
-                {
-                    OptimizeAirControlImpulse(horizontal);
-                }
+                GroundMovement();
+                StartSwing();
             }
         }
         else if (isAnchored)
         {
-            rb.velocity = Vector2.zero;
-            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+           Anchor();
         }
     }
 
-    bool IsNextToWall()
-    {
-        return Physics2D.OverlapCircle(wallCheckUpper.position, 0.1f, wallLayer) || Physics2D.OverlapCircle(wallCheckLower.position, 0.1f, wallLayer);
-    }
 
-    void OptimizeAirControlImpulse(float direction)
+    #endregion
+
+    #region GroundMovement Methods
+    void GroundMovement()
     {
-        float currentHorizontalVelocity = rb.velocity.x;
-        if ((direction > 0 && currentHorizontalVelocity > 0) || (direction < 0 && currentHorizontalVelocity < 0))
+        if (isGroundedThisFrame || Time.time - lastJumpTime < swingCooldown)
         {
-            rb.AddForce(new Vector2(playerStats.airControl * direction, 0), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(horizontal * playerStats.speed, rb.velocity.y);
         }
     }
     #endregion
 
+
     #region Climbing Methods
+       private void HandleClimbing()
+    {
+        bool nextToWall = IsNextToWall();
+
+        if (!isClimbing && climbBufferCounter > 0 && nextToWall)
+        {
+            StartClimbing();
+        }
+
+        if (isClimbing && nextToWall)
+        {
+            ClimbingPhysics();
+
+            float climbingSpeed = CalculateClimbingSpeed();
+            rb.velocity = new Vector2(0, vertical * climbingSpeed);
+        }
+        else if (!nextToWall)
+        {
+            StopClimbing();
+        }
+    }
+    bool IsNextToWall()
+    {
+        return Physics2D.OverlapCircle(wallCheckUpper.position, 0.1f, wallLayer) || Physics2D.OverlapCircle(wallCheckLower.position, 0.1f, wallLayer);
+    }
+    private void ClimbingPhysics()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+        rb.gravityScale = 0;
+    }
+
+    private float CalculateClimbingSpeed()
+    {
+        bool isWallAbove = Physics2D.OverlapCircle(wallCheckUpper.position, 0.3f, wallLayer);
+        bool isWallBelow = Physics2D.OverlapCircle(wallCheckLower.position, 0.3f, wallLayer);
+        
+        if (vertical > 0 && isWallAbove)
+        {
+            return playerStats.climbingSpeedUp;
+        }
+        else if (vertical < 0 && isWallBelow)
+        {
+            return playerStats.climbingSpeedDown;
+        }
+        return 0; 
+    }
     void StartClimbing()
     {
         isClimbing = true;
@@ -183,9 +201,10 @@ public class PlayerMovement : MonoBehaviour
         rb.constraints &= ~RigidbodyConstraints2D.FreezePositionX;
         rb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        
+       
     }
     #endregion
+
 
     #region Jump Methods
     void PerformJump()
@@ -196,12 +215,40 @@ public class PlayerMovement : MonoBehaviour
     }
     #endregion
 
+   #region Swing Methods
+    void StartSwing()
+    {
+        if (!(isGroundedThisFrame || Time.time - lastJumpTime < swingCooldown) && Mathf.Abs(horizontal) > 0.01f)
+        {
+        AirControlImpulse(horizontal);
+        }
+    }
+    void AirControlImpulse(float direction)
+    {
+        float currentHorizontalVelocity = rb.velocity.x;
+        if ((direction > 0 && currentHorizontalVelocity > 0) || (direction < 0 && currentHorizontalVelocity < 0))
+        {
+            rb.AddForce(new Vector2(playerStats.airControl * direction, 0), ForceMode2D.Impulse);
+        }
+    }
+    #endregion
+
+    #region Anchor Methods
+    void Anchor()
+    {
+        rb.velocity = Vector2.zero;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+    #endregion
+
+
     #region Input Actions
     public void Move(InputAction.CallbackContext context)
     {
         Vector2 moveInput = context.ReadValue<Vector2>();
         horizontal = moveInput.x;
         vertical = moveInput.y;
+
 
         if (isGroundedThisFrame && moveInput.y < 0 && !isAnchored)
         {
@@ -220,16 +267,20 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!playerStats.canJump || isAnchored || isClimbing) return;
 
+
         if (context.performed)
         {
             lastJumpTime = Time.time;
             jumpBufferCounter = playerStats.jumpBufferTime;
         }
 
-        if ((isGroundedThisFrame || playerStats.canDoubleJump) && jumpBufferCounter > 0 && !isJumping)
+
+        if ((isGroundedThisFrame || playerStats.canDoubleJump || coyoteTimeCounter > 0) && jumpBufferCounter > 0 && !isJumping)
         {
             PerformJump();
+            coyoteTimeCounter = 0;
         }
+
 
         if (context.canceled && rb.velocity.y > 0f)
         {
@@ -250,3 +301,5 @@ public class PlayerMovement : MonoBehaviour
     }
     #endregion
 }
+
+
